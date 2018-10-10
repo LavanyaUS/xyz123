a1a6c42c1747bd638cb8bcdb11f6b235d8c1a9b2
lavan <lavan@DESKTOP-E9U7U8V>
2018-10-10 10:17:45 -0400

    Commiting by Lavanya

---
+++ Lavanya1/src/Sample_Package1/Sample_Class1.java
@@ -0,0 +1,22 @@
+package Sample_Package1;
+
+public class Sample_Class1 {
+	
+	public void method1()
+	{
+		System.out.println("Lavanya");
+		
+	}
+	public void method2()
+	{
+		System.out.println("Lavanya10");
+	}
+	
+	public static void main(String[] args)		
+	{
+		Sample_Class1 obj=new Sample_Class1();
+		obj.method1();
+		obj.method2();
+	}
+
+}
